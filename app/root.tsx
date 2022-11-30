9fdc4f18fedadeeed51de7cf5fba4cfd3b998e0c
Vinicius Caceres <vinicius.caceres@pmsma.local>
2022-11-30 17:26:45 -0300

    first

--- app/root.tsx
+++ app/root.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+import { useLocation, useMatches } from "@remix-run/react";
 import type { MetaFunction } from "@remix-run/node";
 import remixImageStyles from "remix-image/remix-image.css";
 import {
@@ -10,39 +12,70 @@ import {
 } from "@remix-run/react";
 import Footer from "./components/Layout/Footer/Footer";
 import WhatsAppPopup from "./components/Popup/WhatsAppPopup";
-
 import styles from "./styles/app.css";
+
+let isMount = true;
 
 export function links() {
   return [
     { rel: "stylesheet", href: remixImageStyles },
     { rel: "stylesheet", href: styles },
+    { rel: "manifest", href: "/resources/manifest.webmanifest" },
   ];
 }
-
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
   title: "Conceição Advogados",
   viewport: "width=device-width,initial-scale=1",
 });
-
 export default function App() {
+  let location = useLocation();
+  let matches = useMatches();
+
+  React.useEffect(() => {
+    let mounted = isMount;
+    isMount = false;
+    if ("serviceWorker" in navigator) {
+      if (navigator.serviceWorker.controller) {
+        navigator.serviceWorker.controller?.postMessage({
+          type: "REMIX_NAVIGATION",
+          isMount: mounted,
+          location,
+          matches,
+          manifest: window.__remixManifest,
+        });
+      } else {
+        let listener = async () => {
+          await navigator.serviceWorker.ready;
+          navigator.serviceWorker.controller?.postMessage({
+            type: "REMIX_NAVIGATION",
+            isMount: mounted,
+            location,
+            matches,
+            manifest: window.__remixManifest,
+          });
+        };
+        navigator.serviceWorker.addEventListener("controllerchange", listener);
+        return () => {
+          navigator.serviceWorker.removeEventListener(
+            "controllerchange",
+            listener
+          );
+        };
+      }
+    }
+  }, [location]);
+
   return (
     <html lang="pt-br">
       <head>
-        <Meta />
-        <Links />
+        <Meta /> <Links />
       </head>
-
       <body className=" max-w-[100vw] overflow-x-hidden">
         <main>
-          <WhatsAppPopup />
-          <Outlet />
+          <WhatsAppPopup /> <Outlet />
         </main>
-        <Footer />
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
+        <Footer /> <ScrollRestoration /> <Scripts /> <LiveReload />
       </body>
     </html>
   );
