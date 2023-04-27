ff71d5e841fa5eff1fffd5ce6f558f4e995fe365
viniciuscacere <viniciuscaceres@gmail.com>
2023-04-26 22:08:05 -0300

    feat: add new text to blog

--- app/routes/blog/index.tsx
+++ app/routes/blog/index.tsx
@@ -21,7 +21,7 @@ export default function Blog() {
 
   return (
     <div className="mx-auto flex w-screen gap-x-7 bg-white">
-      <aside className=" mt-16 ml-auto mr-16 max-w-screen-xl flex-col items-center hidden md:flex  ">
+      <aside className=" mt-16 ml-auto mr-16 max-w-screen-xl flex-col items-center px-4 hidden md:flex  ">
         <ul className=" mx-auto flex-col flex justify-center gap-y-3">
           <li className=" mx-auto text-2xl">Últimas Postagens</li>
           <li>
@@ -54,6 +54,15 @@ export default function Blog() {
               className=" text-lg font-bold mt-2 underline text-brown hover:text-gray hover:no-underline"
             >
               Holding familiar
+            </Link>{" "}
+          </li>
+          <li className=" max-w-xs">
+            <Link
+              to={"/blog/busca-e-apreensao-de-veiculos"}
+              className=" text-lg font-bold mt-2 underline text-brown hover:text-gray hover:no-underline"
+            >
+              Tudo o que você precisa saber sobre juros abusivos e busca e
+              apreensão de veículos financiados
             </Link>{" "}
           </li>
         </ul>
@@ -194,39 +203,73 @@ export default function Blog() {
 
           <section className=" flex flex-col justify-center items-center mx-auto">
             {menu === "Direito Civil" && (
-              <article>
-                <div className=" mb-10 flex flex-wrap gap-x-10 justify-center px-10 md:px-0">
-                  <img
-                    src="https://res.cloudinary.com/deaejawfj/image/upload/c_scale,w_440/v1670850655/revisionalCapa.webp"
-                    width={300}
-                    height={300}
-                    alt="Imagem de Revisional de Juros Abusivos"
-                  />
-                  <div>
-                    <h2 className=" text-lg font-bold">
-                      <Link
-                        className=" text-lg font-bold mt-2 underline text-brown hover:text-gray hover:no-underline"
-                        to="/blog/revisional-de-juros-abusivos"
-                      >
-                        Revisional de Juros Abusivos
-                      </Link>
-                    </h2>
-                    <p className=" text-sm mb-2 text-grulo">
-                      Postado em 12/12/22
-                    </p>
-                    <p className=" max-w-lg">
-                      Você esta financiando um imóvel ou um veículo e percebeu
-                      que o valor dos juros cobrados nas prestações são muito
-                      altos? O banco esta cobrando juros abusivos nas faturas
-                      atrasadas do seu cartão de crédito? Você sabia que é
-                      direito seu pedir revisão dos juros e, também, o
-                      ressarcimento por cobranças indevidas? Nesse artigo, você
-                      vai entender o que é a ação revisional, em quais casos ela
-                      se aplica e como solicitar. Confira!
-                    </p>
+              <>
+                <article>
+                  <div className=" mb-10 flex flex-wrap gap-x-10 justify-center px-10 md:px-0">
+                    <img
+                      src="https://res.cloudinary.com/deaejawfj/image/upload/c_scale,w_440/v1670850655/revisionalCapa.webp"
+                      width={300}
+                      height={300}
+                      alt="Imagem de Revisional de Juros Abusivos"
+                    />
+                    <div>
+                      <h2 className=" text-lg font-bold">
+                        <Link
+                          className=" text-lg font-bold mt-2 underline text-brown hover:text-gray hover:no-underline"
+                          to="/blog/revisional-de-juros-abusivos"
+                        >
+                          Revisional de Juros Abusivos
+                        </Link>
+                      </h2>
+                      <p className=" text-sm mb-2 text-grulo">
+                        Postado em 12/12/22
+                      </p>
+                      <p className=" max-w-lg">
+                        Você esta financiando um imóvel ou um veículo e percebeu
+                        que o valor dos juros cobrados nas prestações são muito
+                        altos? O banco esta cobrando juros abusivos nas faturas
+                        atrasadas do seu cartão de crédito? Você sabia que é
+                        direito seu pedir revisão dos juros e, também, o
+                        ressarcimento por cobranças indevidas? Nesse artigo,
+                        você vai entender o que é a ação revisional, em quais
+                        casos ela se aplica e como solicitar. Confira!
+                      </p>
+                    </div>
                   </div>
-                </div>
-              </article>
+                </article>
+                <article>
+                  <div className=" mb-10 flex flex-row-reverse flex-wrap gap-x-10 justify-center px-10 md:px-0">
+                    <img
+                      src="https://res.cloudinary.com/deaejawfj/image/upload/c_scale,w_440/v1670850655/revisionalCapa.webp"
+                      width={300}
+                      height={300}
+                      alt="Imagem ilustrando uma busca e apreensão de veículo"
+                    />
+                    <div>
+                      <h2 className=" text-lg font-bold max-w-md">
+                        <Link
+                          className=" text-lg font-bold mt-2 underline text-brown hover:text-gray hover:no-underline"
+                          to={"/blog/busca-e-apreensao-de-veiculos"}
+                        >
+                          Tudo o que você precisa saber sobre juros abusivos e
+                          busca e apreensão de veículos financiados
+                        </Link>
+                      </h2>
+                      <p className=" text-sm mb-2 text-grulo">
+                        Postado em 24/04/2023
+                      </p>
+                      <p className=" max-w-lg">
+                        Preparamos um guia sobre juros abusivos e busca e
+                        apreensão de veículos financiados. Se você está
+                        enfrentando dificuldades para pagar as parcelas do seu
+                        financiamento de veículo, ou se o seu carro já foi alvo
+                        de uma ação de busca e apreensão, este texto é para
+                        você.
+                      </p>
+                    </div>
+                  </div>
+                </article>
+              </>
             )}
             {menu === "Direito Previdenciário" && (
               <article>
