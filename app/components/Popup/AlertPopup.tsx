8b27c7ca7cd0a1edc2502eeab0c69522511159f9
Vinicius Cáceres <81455169+Viniciusmbc@users.noreply.github.com>
2023-11-24 19:32:38 +0000

    fix: alert popup

--- app/components/Popup/AlertPopup.tsx
+++ app/components/Popup/AlertPopup.tsx
@@ -16,15 +16,16 @@ const AlertPopup: FC<AlertPopupProps> = ({ isOpen, images, onClose }) => {
     <div className="bg-white rounded-lg shadow-xl md:w-[80%]">
       <div className="flex justify-between items-center p-5 rounded-t border-b">
        {exclamationIcon}
-        <h3 className="text-xl font-medium text-gray-900">
+        <h3 className="text-xl font-extrabold text-red-500 ">
           Atenção: WhatsApp Clonado!
         </h3>
       </div>
       <div className="p-5">
-        <p>Recebemos informações de que nosso WhatsApp foi clonado. Por favor, esteja ciente de que nosso escritório de advocacia possui apenas um número oficial de WhatsApp. Não realizamos pedidos de dinheiro ou cobranças fora dos canais oficiais de comunicação.</p>
+        <p>Recebemos informações de que nosso <b>WhatsApp foi clonado</b>. Por favor, esteja ciente de que nosso escritório de advocacia possui apenas um número oficial de WhatsApp. Não realizamos pedidos de dinheiro ou cobranças fora dos canais oficiais de comunicação.</p>
+        <b>Fotos do número de WhatsApp que está se passando por nosso Escritório:</p>
         {images && images.length > 0 && (
-          <div className="mt-4">
-            <p>Fotos do número de WhatsApp que está se passando por nosso Escritório:</p>
+          <div className="mt-4 flex  justify-center items-center">
+           
             {/* Renderizar as imagens */}
             {images.map((img, index) => (
               <img key={index} src={img} alt="WhatsApp clonado" className="mt-2 flex justify-center items-center  w-[30%] max-w-full" />
@@ -32,7 +33,7 @@ const AlertPopup: FC<AlertPopupProps> = ({ isOpen, images, onClose }) => {
           </div>
         )}
       </div>
-      <div className="flex justify-end items-center p-5 rounded-b border-t">
+      <div className="flex items-center p-5 rounded-b border-t">
         <button
           onClick={onClose}
           className="px-4 py-2 bg-red-500 text-white text-base font-medium rounded-md hover:bg-red-700">
