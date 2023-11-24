9232f50f5a861c70da8c8d4ed5883142c1752ad4
viniciuscacere <viniciuscaceres@gmail.com>
2023-11-24 18:05:44 -0300

    feat: add new alert font-size

--- app/components/Popup/AlertPopup.tsx
+++ app/components/Popup/AlertPopup.tsx
@@ -29,15 +29,15 @@ const AlertPopup: FC<AlertPopupProps> = ({ isOpen, images, onClose }) => {
           Nosso <span className='text-[#075e54] font-extrabold'>WhatsApp</span> <b>foi</b>  Clonado!
         </h3>
       </div>
-      <div className="p-5 space-y-4"> {/* Adicionado espaço entre parágrafos */}
-        <p>Recebemos informações de que nosso WhatsApp foi clonado.</p>
+      <div className="p-5 space-y-4 text-lg"> {/* Adicionado espaço entre parágrafos */}
+        <p >Recebemos informações de que nosso WhatsApp foi clonado.</p>
         <p>Por favor, esteja ciente de que nosso escritório de advocacia possui apenas um número oficial de WhatsApp (+55 55 99256-0707).</p>
         <p>Se você receber uma mensagem de um número diferente, fazendo pedidos de dinheiro ou passando qualquer tipo de informação, <b>não responda</b> e entre em contato conosco <b>imediatamente</b>.</p>
         <p>Estamos trabalhando para resolver o problema o mais rápido possível.</p>
 
-        <p>Imagens do número que está se passando pelo nosso escritório:</p>
+        <p>Imagens do número de WhatsApp que está se passando pelo nosso escritório:</p>
         {images && images.length > 0 && (
-          <div className="mt-4 flex justify-around">
+          <div className="mt-4 flex flex-col md:flex-row justify-around">
             {images.map((img, index) => (
               <img key={index} src={img} alt="WhatsApp clonado" className="mt-2 w-52 max-w-full" />
             ))}
